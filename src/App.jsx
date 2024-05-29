f4fe1bc884b3bd5e66d5672601971578f5b8a4eb
Arjun cv <arjun7180@gmail.com>
2024-05-29 22:27:39 +0530

    made user reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect } from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import { useReducer } from "react";
+
+const initialData = {
+  quesitions: [],
+
+  //loading, ready, error
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, status: "Ready", quesitions: action.payload };
+
+    case "dataFailed":
+      return { ...state, status: "Error" };
+
+    default:
+      throw new Error("Unknown Error");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialData);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  console.log(state);
+
   return (
     <>
       <div className="app">
